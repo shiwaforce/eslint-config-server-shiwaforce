c6d8c7a61027e0d5ccf003902a76024dfea09d0c
sarkiroka <sarki_roka@freemail.hu>
2017-02-21 01:27:34 +0100

    add "errors" ruleset fix self-checking

---
+++ changelog.md
@@ -0,0 +1,9 @@
+# Changelog
+
+## 1.0.1 / 2017.02.21.
+- fix self checking lint config settings
+- fix missed "possible errors" ruleset
+- add this changelog
+
+## 1.0.0 / 2017.02.17.
+- Initial release
--- rules/rules.js
+++ rules/rules.js
@@ -6,10 +6,11 @@ var extend = require('extend');
 var beauty = require('./beauty');
 var complexity = require('./complexity');
 var debugging = require('./debugging');
+var errors = require('./errors');
 var git = require('./git');
 var separator = require('./separator');
 var spacing = require('./spacing');
 var etc = require('./etc');
 
-var rules = extend({}, beauty, complexity, debugging, git, separator, spacing, etc);
+var rules = extend({}, beauty, complexity, debugging, errors, git, separator, spacing, etc);
 module.exports = rules;
